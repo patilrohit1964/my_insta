d235d7755b1faf4b7a8ae6e8398e5903bd19c39c
patilrohit1964 <patilrohit19649@gmail.com>
2025-02-21 00:10:17 +0530

    inc msg routing

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -1,4 +1,5 @@
 const Conversation = require("../models/conversation.model");
+const Messages = require("../models/message.model");
 
 const sendMessage = async (req, res) => {
   try {
@@ -13,6 +14,33 @@ const sendMessage = async (req, res) => {
         participants: [senderId, receiverId],
       });
     }
-    
+    const newMessage = await Messages.create({
+      senderId,
+      receiverId,
+      message,
+    });
+    if (newMessage) conversation.messages.push(newMessage._id);
+    await Promise.all([conversation.save(), newMessage.save()]);
+    // implement socket for real time
+
+    return res.status(201).json({
+      success: true,
+      newMessage,
+    });
+  } catch (error) {}
+};
+
+const getMessages = async (req, res) => {
+  try {
+    const senderId = req.id;
+    const receiverId = req.params.id;
+    const conversation = await Conversation.find({
+      participants: { $all: [senderId, receiverId] },
+    });
+    if (!conversation)
+      return res.status(404).json({ success: false, message: [] });
+    return res
+      .status(200)
+      .json({ success: true, message: conversation?.messages });
   } catch (error) {}
 };
