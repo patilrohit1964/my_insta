18b73eb1ee84e6186bbcf4772cb74a325442cb48
patilrohit1964 <patilrohit19649@gmail.com>
2025-02-22 23:47:42 +0530

    added ui

--- client/src/components/ui/provider.jsx
+++ client/src/components/ui/provider.jsx
@@ -3,7 +3,7 @@
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 import { ColorModeProvider } from './color-mode'
 
-export function Provider(props) {
+export default function Provider(props) {
   return (
     <ChakraProvider value={defaultSystem}>
       <ColorModeProvider {...props} />
