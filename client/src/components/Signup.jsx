330cfa5518b3eb1820a3303d8d9173287732434d
patilrohit1964 <patilrohit19649@gmail.com>
2025-02-22 23:51:46 +0530

    create signup ui

---
+++ client/src/components/Signup.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+      <div className='flex items-center w-screen h-screen justify-center'>
+        <form action="">
+          
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Signup
