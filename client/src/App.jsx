851a5b1a52baba8275a6dece08a74bf47d7d7244
patilrohit1964 <patilrohit19649@gmail.com>
2025-02-23 00:08:18 +0530

    ui error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Button, HStack } from "@chakra-ui/react"
+import Signup from './components/Signup'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <HStack>
-
-    </HStack>
+    <>
+      <Signup />
+    </>
   )
 }
 
--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
-import { Input } from "@chakra-ui/react"
+import { Input, Field } from "@chakra-ui/react"
 const Signup = () => {
   return (
     <div>
-      <div className='flex items-center w-screen h-screen justify-center'>
-        <form action="" className='shadow-lg flex flex-col gap-5 p-8'>
-          <div>
+      <div className='flex items-center  h-screen justify-center'>
+        <form action="" className='shadow-lg flex flex-col gap-5 p-8 border border-orange-800'>
+          <div className='my-4'>
             <h1>logo</h1>
             <p>Signup to see photos videos from your friends</p>
           </div>
           <div>
-            <label htmlFor="username">Username</label>
-            <Input type="text" className='focus-visible:ring-transparent' name='username' />
+            <Field.Root>
+              <Field.Label htmlFor="username" className='font-medium'>Username</Field.Label>
+              <Input type="text" className='focus-visible:ring-transparent my-2' name='username' />
+            </Field.Root>
+          </div>
+          <div>
+            <Field.Root>
+              <Field.Label htmlFor="username" className='font-medium'>Email</Field.Label>
+              <Input type="email" className='focus-visible:ring-transparent my-2' name='username' />
+            </Field.Root>
+          </div>
+          <div>
+            <Field.Root>
+              <Field.Label htmlFor="username" className='font-medium'>Username</Field.Label>
+              <Input type="text" className='focus-visible:ring-transparent my-2' name='username' />
+            </Field.Root>
           </div>
         </form>
       </div>
