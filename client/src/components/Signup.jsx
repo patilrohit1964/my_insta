20f36d299eb334ccd16599574d0c8bdbceed2a67
patilrohit1964 <patilrohit19649@gmail.com>
2025-02-22 23:58:29 +0530

    signup ready

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-
+import { Input } from "@chakra-ui/react"
 const Signup = () => {
   return (
     <div>
       <div className='flex items-center w-screen h-screen justify-center'>
-        <form action="">
-          
+        <form action="" className='shadow-lg flex flex-col gap-5 p-8'>
+          <div>
+            <h1>logo</h1>
+            <p>Signup to see photos videos from your friends</p>
+          </div>
+          <div>
+            <label htmlFor="username">Username</label>
+            <Input type="text" className='focus-visible:ring-transparent' name='username' />
+          </div>
         </form>
       </div>
     </div>
