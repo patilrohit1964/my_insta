f103408269f7da52feb1b56b9cb67309497db1a6
patilrohit1964 <patilrohit19649@gmail.com>
2025-02-23 20:30:34 +0530

    error in ui

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -3,11 +3,11 @@ import { Input, Field } from "@chakra-ui/react"
 const Signup = () => {
   return (
     <div>
-      <div className='flex items-center  h-screen justify-center'>
+      <div className='flex items-center h-screen justify-center'>
         <form action="" className='shadow-lg flex flex-col gap-5 p-8 border border-orange-800'>
           <div className='my-4'>
             <h1>logo</h1>
-            <p>Signup to see photos videos from your friends</p>
+            <p className='text-red-600 p-20'>Signup to see photos videos from your friends</p>
           </div>
           <div>
             <Field.Root>
