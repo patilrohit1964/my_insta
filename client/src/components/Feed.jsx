dbf97a8f86fed757d60d8614192b5f1c83f1207f
patilrohit1964 <patilrohit19649@gmail.com>
2025-02-25 23:57:58 +0530

    added ui

---
+++ client/src/components/Feed.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Post from './Post'
+
+const Feed = () => {
+    return (
+        <div className='flex-1 my-0 flex flex-col items-center pl-[20%]'>
+            <Post />
+        </div>
+    )
+}
+
+export default Feed
