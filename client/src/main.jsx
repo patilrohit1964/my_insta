f9aadd6588eb8de5c98c5f5758ce48d61f815a4c
patilrohit1964 <patilrohit19649@gmail.com>
2025-02-25 23:25:28 +0530

    added ui

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { ToastContainer } from "react-toastify"
-import { Provider as ChakraProvider } from "@/components/ui/provider"
+import { Provider as ChakraProvider } from "./components/ui/provider"
 import App from './App.jsx'
 import './index.css'
 import appStore from './redux/store.js'
