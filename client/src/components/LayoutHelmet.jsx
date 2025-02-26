e327cd4ca33eec53860cd20bee38a5234d54366f
patilrohit1964 <patilrohit19649@gmail.com>
2025-02-27 00:24:45 +0530

    added

---
+++ client/src/components/LayoutHelmet.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Helmet } from "react-helmet"
+const LayoutHelmet = ({ title, description, children }) => {
+    return (
+        <div>
+            <Helmet>
+                <title>{title}</title>
+                <meta name="description" content={description} />
+            </Helmet>
+            {children}
+        </div>
+    )
+}
+
+export default LayoutHelmet
