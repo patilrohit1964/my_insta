4f0f63e3538663c623ad6e9705ad5a3df562f1b1
patilrohit1964 <patilrohit19649@gmail.com>
2025-02-27 23:43:38 +0530

    added ui

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,25 +1,44 @@
-import React from 'react'
-import { MoreHorizontal } from 'lucide-react'
-import { Avatar, For, HStack } from "@chakra-ui/react"
-import LayoutHelmet from './LayoutHelmet'
+import React, { useState } from 'react';
+import { MoreHorizontal } from 'lucide-react';
+import { Avatar, Button, useDisclosure } from "@chakra-ui/react";
+import LayoutHelmet from './LayoutHelmet';
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalCloseButton } from "@chakra-ui/modal";
+
 const Post = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
     return (
         <LayoutHelmet title={"Post"} description={"this is Post"}>
             <div className='my-8 w-full max-w-sm mx-auto'>
                 <div className='flex items-center justify-between'>
                     <div className='flex items-center gap-2'>
-                        <Avatar.Root>
-                            <Avatar.Fallback name="Rohit Patil" />
-                            <Avatar.Image src="https://bit.ly/sage-adebayo" />
-                        </Avatar.Root>
+                        <Avatar name="Rohit Patil" src="https://bit.ly/sage-adebayo" />
                         <h1>username</h1>
                     </div>
+
+                    {/* Clickable Icon to Open Dialog */}
+                    <MoreHorizontal size="24px" cursor="pointer" onClick={onOpen} />
+
+                    {/* Chakra UI Modal */}
+                    <Modal isOpen={isOpen} onClose={onClose}>
+                        <ModalOverlay />
+                        <ModalContent>
+                            <ModalHeader>Action</ModalHeader>
+                            <ModalCloseButton />
+                            <ModalBody>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                                    do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                </p>
+                            </ModalBody>
+                        </ModalContent>
+                    </Modal>
                 </div>
+
                 <img src="https://bit.ly/sage-adebayo" alt="" />
             </div>
         </LayoutHelmet>
-    )
+    );
+};
 
-}
-
-export default Post
+export default Post;
--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import Post from './Post'
-import LayoutHelmet from './LayoutHelmet'
 
 const Posts = () => {
     return (
-        <LayoutHelmet title={"Posts"} description={"this is Posts"}>
-            <div>
-                {
-                    [1, 2, 3, 4, 5].map((el, idx) => (
-                        <Post key={idx} />
-                    ))
-                }
-            </div>
-        </LayoutHelmet>
+        <div>
+            {
+                [1, 2, 3, 4, 5].map((el, idx) => (
+                    <Post key={idx} />
+                ))
+            }
+        </div>
     )
 }
 
