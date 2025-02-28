0f6e364a21f05377864820e5817d76ed1fd86172
patilrohit1964 <patilrohit19649@gmail.com>
2025-02-28 23:49:07 +0530

    added comment ui

--- client/src/components/CommentDialog.jsx
+++ client/src/components/CommentDialog.jsx
@@ -1,12 +1,31 @@
-import { Dialog, DialogContent } from '@mui/material'
+import { Dialog, DialogContent, Avatar } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 const CommentDialog = ({ openComment, setOpenComment }) => {
     return (
         <div>
             <Dialog open={openComment}>
-                <DialogContent>
-                    <img src="https://bit.ly/sage-adebayo" />
+                <DialogContent className='max-w-5xl p-0 flex flex-col'>
+                    <div className='flex flex-1'>
+                        <div className='w-1/2'>
+                            <img src="https://bit.ly/sage-adebayo" className='w-full h-full object-cover rounded-l-lg' />
+                        </div>
+                        <div className='w-1/2 flex flex-col justify-between'>
+                            <div className='flex items-center justify-between p-4'>
+                                <div className='flex gap-3 items-center'>
+                                    <Link>
+                                        <Avatar src='https://bit.ly/sage-adebayo' />
+                                    </Link>
+                                    <div>
+                                        <Link className='font-semibold text-xs'>username</Link>
+                                        <span>Bio here...</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </DialogContent>
             </Dialog>
         </div>
