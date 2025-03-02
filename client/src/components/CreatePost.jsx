676b284102c25b07268cd20a53c922dad61e1718
patilrohit1964 <patilrohit19649@gmail.com>
2025-03-03 00:06:28 +0530

    create post done imp only api now

--- client/src/components/CreatePost.jsx
+++ client/src/components/CreatePost.jsx
@@ -8,6 +8,7 @@ const CreatePost = ({ open, setOpen }) => {
     const [caption, setCaption] = useState();
     const [imagePreview, setImagePreview] = useState(null);
     const createPostHandler = (e) => {
+        console.log(file,caption)
         try {
 
         } catch (error) {
@@ -20,7 +21,6 @@ const CreatePost = ({ open, setOpen }) => {
         if (file) {
             setFile(file);
             const image = URL.createObjectURL(file)
-            console.log()
             setImagePreview(image)
         }
     }
@@ -29,7 +29,7 @@ const CreatePost = ({ open, setOpen }) => {
             <Dialog open={open} onClose={() => setOpen(false)} fullWidth>
                 <DialogContent>
                     <DialogTitle className='text-center font-semibold'>Create New Post</DialogTitle>
-                    <div onClick={createPostHandler} className='flex gap-3 items-center'>
+                    <div className='flex gap-3 items-center'>
                         <Avatar src='' />
                         <div>
                             <h1 className='font-semibold text-xs'>Username</h1>
@@ -37,12 +37,20 @@ const CreatePost = ({ open, setOpen }) => {
                         </div>
                     </div>
                     <div className='flex flex-col w-full mt-6'>
-                        <textarea rows={5} className='focus-visible:ring-transparent outline-none border border-blue-400 p-3 rounded w-full' placeholder='Write a caption...' />
-                        <div className='border border-red-500'>
-                            {imagePreview && <img src={imagePreview} alt='Preview' />}
-                        </div>
+                        <textarea rows={5} className='focus-visible:ring-transparent outline-none border border-blue-400 p-3 rounded w-full' placeholder='Write a caption...' onChange={(e)=>setCaption(e.target.value)} />
+                        {
+                            imagePreview &&
+                            <div className='border border-red-500 h-56 w-full'>
+                                <img src={imagePreview} alt='Preview' className='w-full h-full object-cover' />
+                            </div>
+                        }
                         <input ref={imageRef} type="file" className='hidden' onChange={fileChangeHandler} />
                         <button onClick={() => imageRef.current.click()} variant='outline' className='w-fit mx-auto bg-[#0095f6] hover:bg-[#258bcf] p-3 rounded-md text-white mt-3'>Select from computer</button>
+                        {
+                            imagePreview && (
+                                <button variant='outline' className='w-full mx-auto p-3 rounded-md text-white mt-3 bg-[#222d42] hover:bg-[#364052]' onClick={createPostHandler}>Post</button>
+                            )
+                        }
                     </div>
                 </DialogContent>
             </Dialog>
