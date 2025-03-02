e19af73ef544299ec4be845f57de72fef5362c27
patilrohit1964 <patilrohit19649@gmail.com>
2025-03-02 20:29:01 +0530

    persist reducer

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,17 @@ import { ToastContainer } from "react-toastify"
 import { Provider as ChakraProvider } from "./components/ui/provider"
 import App from './App.jsx'
 import './index.css'
-import appStore from './redux/store.js'
+import { appStore, persistor } from './redux/store.js'
+import { PersistGate } from 'redux-persist/lib/integration/react'
+
 createRoot(document.getElementById('root')).render(
   <>
     <Provider store={appStore}>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </PersistGate>
     </Provider>
     <ToastContainer position='top-right' pauseOnHover={true} />
   </>
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,34 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
 import authApi from "./api/authApi";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // Uses localStorage by default
 
+const persistConfig = {
+  key: "root", // Key for the storage
+  storage, // Storage engine (localStorage by default)
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 const appStore = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-export default appStore;
+const persistor = persistStore(appStore);
+
+export { appStore, persistor };
