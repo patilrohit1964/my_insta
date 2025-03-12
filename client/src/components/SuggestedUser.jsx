92a156ec96b2ddc3f970d18485bbeb47b5042bf0
patilrohit1964 <patilrohit19649@gmail.com>
2025-03-12 14:00:18 +0530

    complete suggested api

--- client/src/components/SuggestedUser.jsx
+++ client/src/components/SuggestedUser.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import LayoutHelmet from './LayoutHelmet'
 
-const SuggestedUser = ({ suggestedUser }) => {
-    console.log(suggestedUser);
+const SuggestedUser = ({ suggestedUsers }) => {
+
     return (
         <LayoutHelmet title={"Suggested Users"} description={"Suggested users details"}>
             <div>
@@ -13,12 +13,12 @@ const SuggestedUser = ({ suggestedUser }) => {
                     </div>
                     <div>
                         {
-                            suggestedUser?.map((user, index) => (
+                            suggestedUsers?.map((user, index) => (
                                 <div key={index} className='flex items-center gap-3 my-3'>
                                     <img src='https://bit.ly/sage-adebayo' alt='User' className='w-12 h-12 rounded-full' />
                                     <div className='flex-1'>
                                         <h3 className='font-semibold text-sm'>{user?.username}</h3>
-                                        <p className='text-xs text-gray-500'>Bio: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                        <p className='text-xs text-gray-500'>Bio: {user?.bio}</p>
                                     </div>
                                     <button className='bg-blue-500 text-white font-semibold py-1 px-2 rounded-full'>Follow</button>
                                 </div>
