0eee8e23e42c851aa856c95931c7dd64e8f2408e
patilrohit1964 <patilrohit19649@gmail.com>
2025-03-16 00:08:44 +0530

    edit profile pending

--- client/src/components/EditProfile.jsx
+++ client/src/components/EditProfile.jsx
@@ -7,17 +7,18 @@ const EditProfile = () => {
     const { user } = useSelector(state => state?.auth);
     return (
         <div className='flex max-w-2xl mx-auto pl-10'>
-            <section>
+            <section className='flex flex-col gap-6 w-full'>
                 <h1 className='font-bold text-xl'>Edit Profile</h1>
                 <div className='w-fit my-10 pr-10'>
-                    <div className='flex items-center gap-3'>
-                        <Link to={`/profile/${user?._id}`} className='hover:opacity-80'>
+                    <div className='flex items-center bg-gray-100 rounded-xl p-4'>
+                        <div className='flex items-center gap-3'>
                             <Avatar src={user?.profilePicture} alt='not found' />
-                        </Link>
-                        <div className='flex flex-col items-center'>
-                            <h1 className='font-bold text-sm'>{user?.username}</h1>
-                            <span className='text-gray-600 text-sm'>{user?.bio || "Bio here..."}</span>
+                            <div className='flex flex-col items-center'>
+                                <h1 className='font-bold text-sm'>{user?.username}</h1>
+                                <span className='text-gray-600 text-sm'>{user?.bio || "Bio here..."}</span>
+                            </div>
                         </div>
+                        <input type="text" name="" id="" />p
                     </div>
                 </div>
             </section>
