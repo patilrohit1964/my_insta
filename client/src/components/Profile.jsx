86441385fc30906d8f5bdcf1d4821f0e40a89303
patilrohit1964 <patilrohit19649@gmail.com>
2025-03-15 18:01:31 +0530

    added comment

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -11,11 +11,12 @@ const Profile = () => {
   const { data, isError } = useGetUserProfile(id);
   console.log(data);
   const isLoggedUser = false
+  const Follow = true
   return (
     <LayoutHelmet title={"Profile"} description={"this is Profile"}>
       <div>
         <div className='flex max-w-5xl justify-center mx-auto pl-10 pt-10'>
-          <div className='grid grid-cols-3 border justify-around'>
+          <div className='grid grid-cols-2 border justify-around'>
             <div className='flex items-center justify-center w-32 h-32'>
               <img src={data?.user?.profilePicture} className='h-full w-full object-center rounded-full' />
             </div>
@@ -29,9 +30,16 @@ const Profile = () => {
                       <Button variant='secondary' className='hover:bg-gray-200'>View archive</Button>
                       <Button variant='secondary' className='hover:bg-gray-200'>Ad tools</Button>
                     </>
-                  ) :
-
-                    <button className='bg-[#0095f6] hover:bg-[#59a8dc] p-2 text-white rounded-md'>Follow</button>
+                  ) : (
+                    Follow ?
+                      <>
+                        <button className='bg-gray-300 hover:bg-gray-600 p-2 rounded-md transition-all duration-300 hover:text-white'>Unfollow</button>
+                        <button className='bg-gray-300 hover:bg-gray-600 p-2 rounded-md transition-all duration-300 hover:text-white'>Message</button>
+                      </>
+                      : (
+                        <button className='bg-[#0095f6] hover:bg-[#59a8dc] p-2 text-white rounded-md'>Follow</button>
+                      )
+                  )
                 }
               </div>
             </div>
