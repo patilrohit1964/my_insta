ca29d1e404a2da2495287d87d0c387014789f202
patilrohit1964 <patilrohit19649@gmail.com>
2025-03-19 22:39:21 +0530

    chat page ui started

--- client/src/components/ChatPage.jsx
+++ client/src/components/ChatPage.jsx
@@ -4,17 +4,25 @@ import { useSelector } from 'react-redux'
 
 const ChatPage = () => {
     const { user, suggestedUsers } = useSelector(state => state.auth);
+    const isOnline = true
     return (
         <div>
-            <div className='flex ml-[18%] h-screen'>
-                <section className='border border-red-500'>
+            <div className='flex ml-[16%] px-3 h-screen border border-blue-500'>
+                <section className='border border-red-500 w-full'>
                     <h1 className='font-bold px-3 mb-4 text-xl'>{user?.username}</h1>
-                    <hr className='mb-4 border-b border-b-gray-600' />
+                    <hr className='mb-4 border border-gray-300' />
                     <div className='overflow-y-auto h-[80vh]'>
                         {suggestedUsers?.map((suggestedUser) => (
                             <div>
                                 <Avatar src={suggestedUser?.profilePicture} />
-                                
+                                <div className='flex flex-col'>
+                                    <span className='font-medium'>{suggestedUser?.username}</span>
+                                    <span className={`text-xs font-bold ${isOnline ? "text-green-600" : "text-red-600"}`}>
+                                        {
+                                            isOnline ? "Online" : "Offline"
+                                        }
+                                    </span>
+                                </div>
                             </div>
                         ))}
                     </div>
