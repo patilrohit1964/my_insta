21813aa7faf752e5836d0adc64d5dcde71a0008d
patilrohit1964 <patilrohit19649@gmail.com>
2025-03-20 23:19:16 +0530

    message ui added

--- client/src/components/Messages.jsx
+++ client/src/components/Messages.jsx
@@ -1,9 +1,19 @@
+import { Avatar } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Messages = ({ selectedUser }) => {
   return (
     <div className='overflow-y-auto flex-1 p-4'>
-
+      <div className='flex justify-center'>
+        <div className='flex flex-col items-center justify-center'>
+          <Avatar src={selectedUser?.profilePicture} />
+          <span>{selectedUser?.username}</span>
+          <Link to={`/${selectedUser?._id}/profile`}>
+            <button>View Profile</button>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
