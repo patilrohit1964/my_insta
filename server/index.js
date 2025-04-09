89e07453d561d9b41120414672fa8830f483ab03
patilrohit1964 <patilrohit19649@gmail.com>
2025-04-09 16:04:57 +0530

    deploying

--- server/index.js
+++ server/index.js
@@ -4,7 +4,10 @@ const dotenv = require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const connectDb = require("./config/db");
 const morgan = require("morgan");
+const path = require("path");
 const { app, server } = require("./socket/socket");
+const _dirname = path.resolve();
+console.log(_dirname);
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
@@ -24,6 +27,12 @@ app.use("/api/v1/user", userRouter);
 app.use("/api/v1/post", postRoute);
 app.use("/api/v1/message", messageRoute);
 const PORT = 4050;
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+});
+
 server.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
